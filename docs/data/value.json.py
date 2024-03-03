c25bfb750e5997ba95d77a4dcc7dbf92b82eb126
Vikas Negi <vikas.negi10@gmail.com>
2024-03-03 03:03:32 +0100

    Fix duplicate entry

--- docs/data/value.json.py
+++ docs/data/value.json.py
@@ -10,6 +10,10 @@ for (curr, amount) in zip(port_curr, port_amount):
     curr_hist = get_hist_market(curr, target_currency, duration)
     curr_dict = get_prices_dict(curr_hist["prices"])
 
+    # Correct for duplicate entry for latest date
+    if len(curr_dict) > int(duration):
+        curr_dict = curr_dict[0:int(duration)]
+
     if breakdown_dict is None:
         breakdown_dict = create_breakdown_dict(curr_dict, port_curr)
 
