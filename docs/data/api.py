468b73c0a1827b0e4b27c8c3840ede1be2aac3c5
Vikas Negi <vikas.negi10@gmail.com>
2024-03-09 23:18:54 +0100

    Add logic to get API key from env. var.

--- docs/data/api.py
+++ docs/data/api.py
@@ -1,14 +1,27 @@
 import requests as rq
 import json
+import os
+
+# Define a custom exception class
+class KeyNotFoundError(Exception):
+    pass
 
 #PUB_URL = "https://api.coingecko.com/api/v3"
 PRO_URL = "https://pro-api.coingecko.com/api/v3"
 
-# Get local Pro API key
+# Get API key from file (when running locally) or from env. variable (during CI)
 def get_key():
-    f = open("/home/vikas/Documents/CG_pro_key.json")
-    key_dict = json.load(f)
-    return key_dict["key"]
+    file = "/home/vikas/Documents/CG_pro_key.json"
+    if os.path.exists(file):
+        f = open(file)
+        key_dict = json.load(f)
+        return key_dict["key"]
+    else:
+        key = os.getenv('CG_PRO_KEY')
+        if key is not None:
+            return key
+        else:
+            raise KeyNotFoundError("API key is not available!")
 
 # Switch between demo and pro accounts
 #use_demo = {"accept": "application/json"}
